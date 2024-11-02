3c35aed128559b735f17d4f0deaa79f86da3c1d2
Gamal Khaled <gamal.khaled11@gmail.com>
2024-11-02 20:23:08 +0200

    adding splash screen to ios

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
-import {HomeScreen} from './src/screens';
-import SplashScreen from 'react-native-splash-screen';
+import { HomeScreen } from "./src/screens";
+import SplashScreen from "react-native-splash-screen";
 
 const App = () => {
   useEffect(() => {
     SplashScreen.hide();
-  }, [])
+  }, []);
 
   return <HomeScreen />;
 };
